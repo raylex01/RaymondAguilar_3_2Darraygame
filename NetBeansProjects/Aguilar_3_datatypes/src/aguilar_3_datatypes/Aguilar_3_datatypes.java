59a8e46d6d0ca0b5e9fafd0c836147283e4f8719
Raymond Aguilar <raylex01@yahoo.com>
2015-09-24 06:25:19 -0500

    this is my popquiz

---
+++ NetBeansProjects/Aguilar_3_datatypes/src/aguilar_3_datatypes/Aguilar_3_datatypes.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aguilar_3_datatypes;
+
+/**
+ *
+ * @author raylex01
+ */
+public class Aguilar_3_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //This is a comment
+        //We will use many comments to explain
+        //to explain our code
+        //This program exploressome of the
+        //primitive data types
+        int i = 0;
+        float a = 0.01f;
+        double b = 0.01;
+        short c = 123;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        /*
+        here we will look at some mathematical operators
+        */
+        // + this adds two numbers and more
+        // - this subtracts two numbers or more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows the remainder
+        System.out.println(1 + " an integer ");
+        System.out.println(a + " a float");
+        System.out.println(b + "a double");
+        System.out.println(c + "a short");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(d4 + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+        System.out.println(5 + " a boolean");
+        for(i = -123;i < 123; i++) {
+            //System.out.println((char) i);
+   //         System.out.println(i % 3== 0);
+        }
+        // a number value for char
+        // maps to a character
+        System.out.println(1 + " an integer ");
+        System.out.println(a + " a float");
+        System.out.println(b + "a double");
+        System.out.println(c + "a short");
+        
+        
+        
+    }
+    
+}
